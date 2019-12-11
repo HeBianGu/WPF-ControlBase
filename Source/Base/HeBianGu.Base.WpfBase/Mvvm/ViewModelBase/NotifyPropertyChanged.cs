1e55089c7109170522a326541b049524b28232d5
河边骨 <navylee0210@163.com>
2019-12-11 11:08:55 +0800

    1111

--- Source/Base/HeBianGu.Base.WpfBase/Mvvm/ViewModelBase/NotifyPropertyChanged.cs
+++ Source/Base/HeBianGu.Base.WpfBase/Mvvm/ViewModelBase/NotifyPropertyChanged.cs
@@ -52,4 +52,82 @@ namespace HeBianGu.Base.WpfBase
     }
 
 
+    //public class ValidateModelBase : NotifyPropertyChanged, IDataErrorInfo
+    //{
+    //    public ValidateModelBase()
+    //    {
+
+    //    }
+
+    //    #region 属性 
+    //    /// <summary>
+    //    /// 表当验证错误集合
+    //    /// </summary>
+    //    private Dictionary<String, String> dataErrors = new Dictionary<String, String>();
+
+    //    /// <summary>
+    //    /// 是否验证通过
+    //    /// </summary>
+    //    public Boolean IsValidated
+    //    {
+    //        get
+    //        {
+    //            if (dataErrors != null && dataErrors.Count > 0)
+    //            {
+    //                return false;
+    //            }
+    //            return true;
+    //        }
+    //    }
+    //    #endregion
+
+    //    public string this[string columnName]
+    //    {
+    //        get
+    //        {
+    //            ValidationContext vc = new ValidationContext(this, null, null);
+    //            vc.MemberName = columnName;
+    //            var res = new List<ValidationResult>();
+    //            var result = Validator.TryValidateProperty(this.GetType().GetProperty(columnName).GetValue(this, null), vc, res);
+    //            if (res.Count > 0)
+    //            {
+    //                AddDic(dataErrors, vc.MemberName);
+    //                return string.Join(Environment.NewLine, res.Select(r => r.ErrorMessage).ToArray());
+    //            }
+    //            RemoveDic(dataErrors, vc.MemberName);
+    //            return null;
+    //        }
+    //    }
+
+    //    public string Error
+    //    {
+    //        get;set;
+    //    }
+
+
+    //    #region 附属方法
+
+    //    /// <summary>
+    //    /// 移除字典
+    //    /// </summary>
+    //    /// <param name="dics"></param>
+    //    /// <param name="dicKey"></param>
+    //    private void RemoveDic(Dictionary<String, String> dics, String dicKey)
+    //    {
+    //        dics.Remove(dicKey);
+    //    }
+
+    //    /// <summary>
+    //    /// 添加字典
+    //    /// </summary>
+    //    /// <param name="dics"></param>
+    //    /// <param name="dicKey"></param>
+    //    private void AddDic(Dictionary<String, String> dics, String dicKey)
+    //    {
+    //        if (!dics.ContainsKey(dicKey)) dics.Add(dicKey, "");
+    //    }
+    //    #endregion
+    //}
+
+
 }
