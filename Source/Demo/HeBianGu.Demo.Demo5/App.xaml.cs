c8ff94d02fcdd5e6e954fa33117d7ac5fdc4fe62
HeBianGu <908293466@qq.com>
2023-08-06 23:42:04 +0800

    5.0.0

--- Source/Demo/HeBianGu.Demo.Demo5/App.xaml.cs
+++ Source/Demo/HeBianGu.Demo.Demo5/App.xaml.cs
@@ -45,23 +45,31 @@ namespace HeBianGu.Demo.Demo5
             //  Do：设置默认主题
             app.UseLocalTheme(l =>
             {
+                //  Do ：设置默认主色调
                 l.AccentColor = (Color)ColorConverter.ConvertFromString("#FF0093FF");
+                //  Do ：设置默认字体大小
                 l.DefaultFontSize = 14D;
+                //  Do ：设置默认字体大小模式
                 l.FontSize = FontSize.Small;
-
+                //  Do ：控件默认高度
                 l.ItemHeight = 36;
+                //  Do ：设置行默认高度
                 l.RowHeight = 40;
-                //l.ItemWidth = 120;
+                //  Do ：设置控件默认圆角
                 l.ItemCornerRadius = 5;
-
+                //  Do ：设置启用动态主色调后动态变化的时间间隔
                 l.AnimalSpeed = 5000;
+                //  Do ：设置主色调列表默认选择项
                 l.AccentColorSelectType = 0;
+                //  Do ：设置是否启用动态主色调
                 l.IsUseAnimal = false;
-
+                //  Do ：设置主题颜色，深色调，浅色调等
                 l.ThemeType = ThemeType.Light;
-
+                //  Do ：设置控件的默认显示样式，Default、Light、Single等
+                l.StyleType = StyleType.Default;
+                //  Do ：设置默认语言（目前不起作用）
                 l.Language = Language.Chinese;
-
+                //  Do ：设置主色调类型，渐变方式等
                 l.AccentBrushType = AccentBrushType.LinearGradientBrush;
             });
         }
